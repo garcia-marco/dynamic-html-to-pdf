432be5058092cb1b83f09f91af302f3b1d20b3cd
garcia-marco <garcia.j.marco@gmail.com>
2023-01-17 13:23:19 +0100

    Change la version de puppeteer

--- index.js
+++ index.js
@@ -32,7 +32,12 @@ module.exports.create = async (template, context, options) => {
     const html = await edge.render(template, context)
 
     // html string to pdf
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
+    })
     const page = await browser.newPage()
     await page.setContent(html, { waitUntil: 'networkidle0' })
     await page.pdf(options)
