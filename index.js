0cd1375f3d256873add21ff84248770cf9761eb3
garcia-marco <garcia.j.marco@gmail.com>
2023-02-13 10:41:11 +0100

    Use only playwright-chromium

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
  */
 
 const { default: edge } = require('edge.js')
-const { chromium } = require('playwright')
+const { chromium } = require("playwright-chromium")
 
 module.exports.create = async (template, context, options) => {
   if (!template) {
@@ -32,7 +32,7 @@ module.exports.create = async (template, context, options) => {
     const html = await edge.render(template, context)
 
     // Html string to pdf
-    const browser = await chromium.launch()
+    const browser = await chromium.launch({ chromiumSandbox: false })
     const page = await browser.newPage()
     await page.setContent(html)
     await page.pdf(options)
